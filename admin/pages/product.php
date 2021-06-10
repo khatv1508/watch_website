5bf2ff4876517a24a52251e857b25a97e1a2da19
khatv1508 <vinhkha1508@gmail.com>
2021-06-10 15:35:00 +0700

    main commit 10/6/2021 chinh sua page san pham va hinh anh

--- admin/pages/product.php
+++ admin/pages/product.php
@@ -161,16 +161,6 @@
             break;
 
         case 'image':
-            // $insert_image = "INSERT INTO anhsanpham(idSP, urlImage) VALUES ($id, $urlImage);";
-            // if (mysqli_query($conn, $insert_image) === TRUE ) {
-            //     echo 
-            //     '<div class="alert alert-success alert-dismissible fade show" role="alert">
-            //         <strong>Upload ảnh thành công</strong>
-            //         <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-            //         <span aria-hidden="true">&times;</span>
-            //         </button>
-            //     </div>';
-            // }
             break;    
         
         default: 
